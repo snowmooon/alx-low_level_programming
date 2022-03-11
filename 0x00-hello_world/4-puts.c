ad64635f108c6576af79a0b65572aa28dfa98563
Snow Moon <snow.moooon@protonmail.com>
2022-03-11 02:11:10 +0000

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+	/* Announcing our string */ 
+	char out[] = "Programming is like building a multilingual puzzle.";
+	
+	/* Writing to stdout */
+	puts(out);
+
+	return (0);
+}
