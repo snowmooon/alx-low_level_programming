4e242071afc63a61d3a8e1b065f886e7ecec461a
Snow Moon <snow.moooon@protonmail.com>
2022-03-11 02:32:22 +0000

    Hello, Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	/* Announcing our string */ 
-	char out[] = "Programming is like building a multilingual puzzle.";
-	
+	/* Announcing our string */
+	char out[] = "\"Programming is like building a multilingual puzzle.";
+
 	/* Writing to stdout */
 	puts(out);
 
